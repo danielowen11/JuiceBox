8cef50977701fdf19a00162030a6ddb22348b985
Daniel Owen <danowen@tutamail.com>
2019-06-03 16:46:48 -0500

    Changed to object-oriented scheme, renamed variables and functions for greater readability.

--- juicebox.py
+++ juicebox.py
@@ -39,83 +39,99 @@ GPIO.output(pin_led_ring, False)
 
 headers = {'authorization': "FLUD_KEY"}
 
+class Juicebox:
 
-def check_status(user, rid):
-    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-    (status, uid) = MIFAREReader.MFRC522_Anticoll()
-    if status == MIFAREReader.MI_OK:
-        heart_beat()
-        rid = str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3])
-        print(user, "RFID:", rid, file=sys.stderr)
-        
+    def __init__(self):    
+        self.rid_1 = ""
+        self.rid_2 = ""
+        self.operator = None
+        self.employee = None
+        self.transaction_id = ""
+        self.phase2 = false
+
+
+    def get_details(self, rid):
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+        if status == MIFAREReader.MI_OK:
+            self.heart_beat()
+            rid = str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3])
+            print(user, "RFID:", rid, file=sys.stderr)
+            
+            try:
+                payload = {"type": "check_status_rfid", "number": rid}
+                r = requests.request("POST", serverURL, json=payload, headers=headers)
+                response = r.json()
+                r.raise_for_status()
+                print(response, file=sys.stderr)
+            except Exception:
+                response = "Check Status: Unable to connect. Verify connection."
+                print(response, file=sys.stderr)
+                return false
+
+            try:
+                json_obj = json.loads(json.dumps(response))
+                print(user, "Level:", json_obj["role"], file=sys.stderr)
+            except Exception:
+                print("JSON: ID parse failure", file=sys.stderr)
+                return false
+
+        return true
+
+
+    def check_if_authorized(self, id_number, id_number_2):
         try:
-            payload = {"type": "check_status_rfid", "number": rid}
+            payload = {"type": "rfid_double", "number": id_number, "number_employee": id_number_2, "device": device_id}
             r = requests.request("POST", serverURL, json=payload, headers=headers)
             response = r.json()
-            r.raise_for_status()
-            print(response, file=sys.stderr)
+
         except Exception:
-            response = "Check Status: Unable to connect. Verify connection."
+            response = "failed to request authorization of 2 different IDs from server.... check connection and url"
             print(response, file=sys.stderr)
             return false
 
+        return true
+
+
+    def end_trans(self):
+        GPIO.output(pin_connect, True)
+        GPIO.output(pin_led_ring, True)
+        trans_id = json_obj[u'trans_id']
+        time.sleep(0.5)
+        GPIO.wait_for_edge(pin_button, GPIO.FALLING)
+        payload = {"type": "end_transaction", "dev_id": device_id}
         try:
-            user_level_json = json.loads(json.dumps(response))
-            print(user, "Level:", user_level_json["role"], file=sys.stderr)
+            r = requests.request("POST", serverURL, json=payload, headers=headers)
+            response = r.json()
+
         except Exception:
-            print("JSON: ID parse failure", file=sys.stderr)
-            return false
+            response = "could not end transaction"
+            print(response, file=sys.stderr)
+            return response
 
-    return true
+        print("End Transaction:", response, file=sys.stderr)
+        GPIO.output(pin_connect, False)
+        GPIO.output(pin_led_ring, False)
 
-
-def authorization_double(id_number, id_number_2):
-    try:
-        payload = {"type": "rfid_double", "number": id_number, "number_employee": id_number_2, "device": device_id}
-        r = requests.request("POST", serverURL, json=payload, headers=headers)
-        response = r.json()
-
-    except Exception:
-        response = "failed to request authorization of 2 different IDs from server.... check connection and url"
-        print(response, file=sys.stderr)
-        return false
-
-    return true
-
-
-def end_trans(json_obj):
-    GPIO.output(pin_connect, True)
-    GPIO.output(pin_led_ring, True)
-    trans_id = json_obj[u'trans_id']
-    time.sleep(0.5)
-    GPIO.wait_for_edge(pin_button, GPIO.FALLING)
-    payload = {"type": "end_transaction", "dev_id": device_id}
-    try:
-        r = requests.request("POST", serverURL, json=payload, headers=headers)
-        response = r.json()
-
-    except Exception:
-        response = "could not end transaction"
-        print(response, file=sys.stderr)
         return response
 
-    print("End Transaction:", response, file=sys.stderr)
-    GPIO.output(pin_connect, False)
-    GPIO.output(pin_led_ring, False)
 
-    return response
+    def heart_beat(self):
+        global pin_led_ring
+        time.sleep(0.5)
+        GPIO.output(pin_led_ring, True)
+        time.sleep(0.25)
+        GPIO.output(pin_led_ring, False)
+        time.sleep(0.25)
+        GPIO.output(pin_led_ring, True)
+        time.sleep(0.25)
+        GPIO.output(pin_led_ring, False)
 
 
-def heart_beat():
-    global pin_led_ring
-    time.sleep(0.5)
-    GPIO.output(pin_led_ring, True)
-    time.sleep(0.25)
-    GPIO.output(pin_led_ring, False)
-    time.sleep(0.25)
-    GPIO.output(pin_led_ring, True)
-    time.sleep(0.25)
-    GPIO.output(pin_led_ring, False)
+    def refresh(self):
+        self.phase2 = false
+
+# end of Juicebox Class
 
 
 # this is from the MFC library, it is to ensure safe exit
@@ -134,42 +150,33 @@ MIFAREReader = MFRC522.MFRC522()
 
 
 def main():
-    temp_rid = ""
-    rid_1 = ""
-    rid_2 = ""
-    has_operator_id = false
-    has_employee_id = false
-    trans_id = ""
-    phase2 = false
 
+    juicebox = Juicebox()
     GPIO.add_event_detect(pin_button, GPIO.FALLING)
 
     while continue_reading:
         if (GPIO.event_detected(pin_button) && phase2 == false): # if the button is pressed
-            has_operator_id = check_status(rid_1)
-            if (has_operator_id):
-                phase2 = true
+            valid_id = juicebox.get_details(juicebox.rid_1) # get operator details
+            if (valid_id):
+                juicebox.phase2 = true # continue on to read for employee details
 
-        if (phase2):
-            has_employee_id = check_status(rid_2)
-            if (has_employee_id):
-                json_obj = authorization_double(rid_1, rid_2)
-                print("Status:",json_obj, file=sys.stderr)
+        if (juicebox.phase2):
+            valid_id = juicebox.get_details(juicebox.rid_2) # get employee details
+            if (valid_id):
+                transaction = juicebox.check_if_authorized(juicebox.rid_1, juicebox.rid_2) # check if the employee ID is the authorized level
+                print("Status:", transaction, file=sys.stderr)
                 try:
-                    if json_obj[u'authorized'] == "Y":
-                        end_obj=end_trans(device_id)
-                        has_operator_id = false
-                        has_employee_id = false
-                        phase2 = false
+                    if transaction[u'authorized'] == "Y": # if the employee is the authorized level
+                        juicebox.end_transaction()
+                        juicebox.refresh()
                         continue
                     else:
-                        heart_beat()
-                        has_operator_id = false
-                        has_employee_id = false
-                        phase2 = false
+                        juicebox.heart_beat()
+                        juicebox.refresh()
                         continue
                 except Exception:
                     print("Error parsing json of the authorization ... check to make sure the server is returning json.", file=sys.stderr)
+                    juicebox.refresh()
                     continue
 
         time.sleep(0.1)
